122e0075b186dc4365fc42d6c3df781909667aa4
s6704062616036-collab <s6704062616036@email.kmutnb.ac.th>
2025-12-26 19:31:07 +0700

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -1,16 +1,3 @@
-//#include <stdio.h>
-//int main (void)
-//{
-//int i,j;
-//for ( i = 1; i <= 3; i++) {	
-//	printf("Row %d: ", i);
-//	for ( j = 1; j<= 5; j++)
-//		printf("%3d", j);
-//		printf("\n");
-//	} // for i
-//return 0;
-//} // main
-
 #include <stdio.h>
 int main (void)
 {
@@ -24,3 +11,4 @@ int main (void)
 		
 	}
 } 
+
